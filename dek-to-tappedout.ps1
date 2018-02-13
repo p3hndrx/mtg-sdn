b1f670687c20ebca63ade7d2d24508aef2af36fd
p3hndrx <richardphung@gmail.com>
2018-02-13 17:31:27 -0500

    New Templates & Dek-to-Tapped Script

---
+++ dek-to-tappedout.ps1
@@ -0,0 +1,25 @@
+﻿$path = "C:\Users\Richard\Google Drive\mtg\mtgo.decks.edh"
+$decks = gci $path
+
+foreach($list in $decks)
+{
+
+    $file = gc $list.fullname
+    
+    #strip head
+    $file = $file -replace ('(?=<\?xml).*(?>\?>)','')
+    $file = $file -replace ('(?=<Deck).*(?>>)','')
+    $file = $file -replace ('(?=<NetDeckID).*(?>NetDeckID>)','')
+    $file = $file -replace ('(?=<PreconstructedDeckID).*(?>ID>)','')
+    #keep quantity
+    $file = $file -replace ('(?=<Cards).*(?>Quantity=")','')
+    $file = $file -replace ('(?=").*(?>Name=")','x ')
+    $file = $file -replace ('" />','')
+    $file = $file -replace ('</Deck>','')
+    
+    #outfile
+    $save = $list.fullname -replace('.dek','.txt')
+    $save = $save -replace ('Google Drive\\mtg\\mtgo.decks.edh\\','Desktop\tapped-out-export\')
+    $save
+    $file.TrimStart() > $save
+}
